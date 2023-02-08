2f3fc432613cda619c7599993b846efcad3b30ee
Examblog <123282721+Examblog@users.noreply.github.com>
2023-02-08 21:34:33 +0700

    Update Examblog.js

--- js/Examblog.js
+++ js/Examblog.js
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 function setting(){
        document.querySelector('.copyright').innerHTML = "Copyright  @ Examblog Created by <a href="https://www.facebook.com/groups/exambloggas"
         target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
