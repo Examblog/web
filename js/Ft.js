a0914aed35b4fa0996819727a7edeee8c4711237
Examblog <123282721+Examblog@users.noreply.github.com>
2023-02-08 21:44:03 +0700

    Update Ft.js

--- js/Ft.js
+++ js/Ft.js
@@ -1,19 +1,3 @@
 function setFooter(){
-  document.getElementById('copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
-  }
-
-function setFooter2(){
-  document.getElementById('copyright2').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
-  }
-
-function setFooter3(){
-  document.getElementById('copyright3').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
-  }
-
-function setFooter4(){
-  document.getElementById('copyright4').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
-  }
-
-function setFooter5(){
-  document.getElementById('copyright5').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
+  document. document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/groups/exambloggas" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2023 <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
   }
